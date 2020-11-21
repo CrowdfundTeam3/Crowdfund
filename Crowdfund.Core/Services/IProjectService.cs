e560c7b40ec06ae452f51152871bb9bc3b3638af
adav <30274421+adavradou@users.noreply.github.com>
2020-11-21 18:59:41 +0200

    Update IProjectService.cs

--- Crowdfund.Core/Services/IProjectService.cs
+++ Crowdfund.Core/Services/IProjectService.cs
@@ -11,9 +11,9 @@ namespace CrowdfundCORE.Services
         ProjectOptions GetProjectById(int projectId);
         List<ProjectOptions> GetProjectsByCreatorId(int creatorId);
         List<ProjectOptions> GetProjectsByBackerId(int backerId);
-        //GetProjectCategory
+        List<ProjectOptions> GetProjectsByCategory(string category);
         List<ProjectOptions> GetProjectsBySearchTerm(string searchTerm);
-
+        List<ProjectOptions> GetMostPopularProjects();
         List<ProjectOptions> GetAllProjects();
         ProjectOptions UpdateProject(ProjectOptions projectOptions, int id);
         bool DeleteProjectWithId(int id);

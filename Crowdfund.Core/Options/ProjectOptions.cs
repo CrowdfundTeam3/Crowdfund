13395b73ccf9c7f3160f2205991d2b38d53cd7a1
Agapi <adavradou@gmail.com>
2020-11-21 18:13:11 +0200

    Add Options UserOptions, ProjectOptions, PackageOptions

---
+++ Crowdfund.Core/Options/ProjectOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace CrowdfundCORE.Options
+{
+    public class ProjectOptions
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Photo { get; set; }
+        public string Video { get; set; }
+        public decimal GoalFund { get; set; }
+        public decimal TotalFund { get; set; }
+    }
+}

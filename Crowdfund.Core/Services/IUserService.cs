4cf635cf0947e51e91dc824ff3c8738797e0fd55
JamesKyrk <james.kyrk.dev@gmail.com>
2020-11-21 17:10:50 +0200

    Add services

---
+++ Crowdfund.Core/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using CrowdfundCORE.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrowdfundCORE.Services
+{
+    public interface IUserService
+    {
+        UserOptions CreateUser(UserOptions userOptions);
+        UserOptions GetUserById(int id);
+        List<UserOptions> GetAllUsers();
+        List<UserOptions> GetBackersByProjectId(int projectId);
+        UserOptions UpdateUserWithId(UserOptions userOptions, int id);
+        bool DeleteUserWithId(int id);
+    }
+}

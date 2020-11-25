80599d9c36df3d77be3f4b19f7ab520ff70e9666
JamesKyrk <james.kyrk.dev@gmail.com>
2020-11-25 20:00:33 +0200

    Navbar footer

--- CrowdFund/wwwroot/js/site.js
+++ CrowdFund/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+if (getUserId()) {
+    $('#logout-btn').show();
+}
+
+function getUserId() {
+    return localStorage.getItem('userId');
+}
+
+// Events
+
+$('#login-btn').on('click', function () {
+    let userEmail = $('#user-email').val();
+    let password = $('#user-password').val();
+
+    let loginOptions = {
+        email: userEmail,
+        password: password
+    };
+
+    $.ajax({
+        url: '/home/login',
+        contentType: 'application/json',
+        type: 'POST',
+        data: JSON.stringify(loginOptions),
+        success: function (data) {
+            localStorage.setItem('userId', data.userId);
+            $('#logout-btn').show();
+        },
+        error: function () {
+            alert('Login denied');
+        }
+    });
+});
+
+$('#logout-btn').on('click', function () {
+    localStorage.removeItem('userId');
+    $('#logout-btn').hide();
+});
